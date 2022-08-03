e6f871f7c49aa662e20204d6f177a40613957ac7
miguel.cardenas <miguel.cardenas@ebcont.com>
2022-08-03 21:27:08 +0200

    kafka producer and consumer setup

---
+++ src/main/java/com/amigoscode/consumer/config/KafkaListeners.java
@@ -0,0 +1,20 @@
+package com.amigoscode.consumer.config;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * Define each listener for each Topic and Read Data!
+ * (kind of like a controller)
+ */
+@Component
+public class KafkaListeners {
+
+    // group id is so that if we have more instances of the same application they can all read from the same partition or topic
+    // so if you have more listeners you want to change ir to a UID
+    @KafkaListener(topics = "amigoscode", groupId = "foo")
+    void listener(String data){
+        // here we can then do anything we want with that data!! e.g. pass it to other components and so on
+        System.out.println("Heyyy! Listener received data: "+ data);
+    }
+}
---
+++ src/main/java/com/amigoscode/producer/config/TopicConfig.java
@@ -0,0 +1,19 @@
+package com.amigoscode.producer.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+/**
+ * responsible for creating topics
+ */
+@Configuration
+public class TopicConfig {
+
+    @Bean
+    public NewTopic amigoscodeTopic() {
+        return TopicBuilder.name("amigoscode")
+                .build();
+    }
+}
